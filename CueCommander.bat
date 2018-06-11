31bc5693576e9c8c4533d3dcf62110bf7aa7a2f5
avmaint <avmaint@uachome.org>
2018-06-11 07:05:13 -0400

    added batch file, updated qlc config

---
+++ CueCommander.bat
@@ -0,0 +1,13 @@
+REM backup qwx file
+
+ipconfig | find "IPv4"
+
+d:
+cd \Dropbox\UAC_Software\CueCommander\
+
+set qxw=CueCommander.qxw
+
+start C:\QLC+\qlcplus.exe -o %qxw%  --operate --web
+
+py \Dropbox\UAC_Software\CueCommander\CueCommander.py
+
