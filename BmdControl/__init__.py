82dd9ca6ed6e0b2d046b34c6cea09ad93a130837
avmaint <avmaint@uachome.org>
2018-06-15 16:47:10 -0400

    removed extraneous print stmt

--- BmdControl/__init__.py
+++ BmdControl/__init__.py
@@ -2,5 +2,3 @@ from Config                 import config
 from osc4py3.as_eventloop import *
 
 osc_udp_client(config.parms["atemOsc_ip"], config.parms["atemOsc_port"], config.parms["atemOsc_cn"])
-
-print("BmdControl init run")
